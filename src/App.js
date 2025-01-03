2aae620fec2354cf42d6f1ad1b2a693f33d361ac
tugsuu511 <tugserdenedamdin@gmail.com>
2025-01-03 11:13:12 +0800

    part1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
+import "./App.css";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
-import './App.css';
+const todo = {
+  text : 'Do homework',
+  id : 1,
+  status : "Active" | "Completed",
+}
+
+
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [deletedTodo, setDeletedTodo] = useState([]);
+  const [error, setError] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  const handleAddButton = () => {
+    if (inputValue.length === 0) {
+      setError("Please enter a todo");
+      return;
+    } else {
+      setError("");
+      setTodo([...todo, { text: inputValue, id: uuidv4(), status: "Active" }]);
+      setInputValue("");
+    }
+  };
+  const handleCheckbox = (id) => {
+    console.log(id);
+    todo.map((todo) => {
+      if (id === todo.id) {
+        todo.status = "Completed";
+      }
+      return todo;
+    });
+    
+
+  }
+console.log(todo);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="...">
+      <input
+        placeholder="Add to do"
+        value={inputValue}
+        onChange={handleInputChange}
+      ></input>
+      {error.length > 1 && <div>{error}</div>}
+      <button onClick={handleAddButton}>Add</button>
+      {todo.map((todo) => {
+        return <div>
+          <input type="checkbox" onChange={handleCheckbox(todo.id)}></input>
+          {todo.text}
+          </div>;
+      })}
     </div>
   );
 }
